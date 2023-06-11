f782f897c4300502b50649b27fc84542cb2aba9e
Ronald Hatcher <ronaldhatcher@mac.com>
2023-06-11 16:47:04 +0100

    relay code cleanup

--- water/water.ino
+++ water/water.ino
@@ -5,33 +5,30 @@
 #define TFT_CS 10
 #define TFT_DC 9
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-Adafruit_FT6206 cts = Adafruit_FT6206();
-
-#define MIN_PRESSURE 10
-#define MAX_PRESSURE 1000
 #define NUM_BUTTONS 3
 #define NUM_SENSORS 2
 
-int buttonHeight = 60;
-int buttonWidth;
-int buttonTop;
-
-int relayPins[3] = {13, 12, 11};
-int productFlowPin = 2;
-int wasteFlowPin = 3;
-
-struct buttonCtrl {
+typedef struct buttonCtrl {
   String label;
   int pin;
   boolean state;
 };
 
-struct sensorCtrl {
+typedef struct sensorCtrl {
   String label;
   int pin;
   volatile int* flowCounter;
 };
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+Adafruit_FT6206 cts = Adafruit_FT6206();
+
+int buttonHeight = 60;
+int buttonWidth;
+int buttonTop;
+
+int productFlowPin = 2;
+int wasteFlowPin = 3;
 
 buttonCtrl buttons[NUM_BUTTONS] = {
   {"Feed", 13, false}, 
@@ -42,7 +39,7 @@ buttonCtrl buttons[NUM_BUTTONS] = {
 volatile int productFlowCounter = 0;
 volatile int wasteFlowCounter = 0;
 
-sensorCtrl sensors[2] = {
+sensorCtrl sensors[NUM_SENSORS] = {
   {"Product", 2, &productFlowCounter},
   {"Waste  ", 3, &wasteFlowCounter}
 };
@@ -66,9 +63,8 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(productFlowPin), productFlowInterrupt, RISING);
   attachInterrupt(digitalPinToInterrupt(wasteFlowPin), wasteFlowInterrupt, RISING);
 
-  tft.begin();
-
   // Rotate the screen 90 degrees
+  tft.begin();
   tft.setRotation(3);
   tft.fillScreen(ILI9341_BLACK);
 
@@ -78,18 +74,26 @@ void setup() {
   } else {
     Serial.println("Touchscreen started.");
   }
+  drawButtons();
+ 
+}
+
+void drawButtons(void) {
 
   buttonWidth = tft.width() / NUM_BUTTONS;
   buttonTop = tft.height() - buttonHeight;
+  
   tft.setTextSize(3);
-  // tft.setFont(&FreeMonoBoldOblique12pt7b);
 
   for (int i = 0; i < NUM_BUTTONS; i++) {
+
     int padding = (buttons[i].label.length() == 4) ? 20 : 10;
-    tft.drawRect(i * buttonWidth, buttonTop, buttonWidth, buttonHeight, ILI9341_WHITE);
     uint16_t color = buttons[i].state ? ILI9341_DARKGREEN : ILI9341_RED;
+
+    tft.drawRect(i * buttonWidth, buttonTop, buttonWidth, buttonHeight, ILI9341_WHITE);
     tft.fillRect(i * buttonWidth + 1, buttonTop + 1, buttonWidth - 1, buttonHeight - 1, color);
     tft.setCursor(i * buttonWidth + padding, buttonTop + 20);
+    tft.setTextColor(ILI9341_WHITE, color);
     tft.println(buttons[i].label);
   }
   tft.setFont();
@@ -106,7 +110,7 @@ void loop() {
   float productFlowRate = productFlowCount / 5880.0;
   float wasteFlowRate = wasteFlowCount / 5880.0;
 
-  // Display the new sensor readings
+  // Display the sensor readings
   tft.setCursor(0, 0);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(3);
@@ -119,25 +123,18 @@ void loop() {
 
   // Check for touch events and handle them
   if (cts.touched()) {
+
     TS_Point p = cts.getPoint();
-    Serial.print("X = ");
-    Serial.print(p.x);
-    Serial.print("\tY = ");
-    Serial.println(p.y);
+    // rotate coordinate system
+    int y = p.x;
+    int x = p.y;
 
-    for (int i = 0; i < 3; i++) {
-      if (p.x >= i * buttonWidth && p.x < (i + 1) * buttonWidth &&
-          p.y >= tft.height() / 2 && p.y < tft.height() / 2 + buttonHeight) {
-        // The touch event was within the area of button i. Toggle the state of
-        // relay i.
-        bool newState = !digitalRead(relayPins[i]);
-        digitalWrite(relayPins[i], newState);
-
-        // Update the color of the button to indicate the new state
-        uint16_t color = newState ? ILI9341_GREEN : ILI9341_RED;
-        tft.fillRect(i * buttonWidth, tft.height() / 2, buttonWidth,
-                     buttonHeight, color);
-      }
+    if (y < buttonHeight) {
+      int index = x / buttonWidth;
+      buttons[index].state = !buttons[index].state;
+      digitalWrite(buttons[index].pin, buttons[index].state);
+      drawButtons();
     }
   }
+
 }
