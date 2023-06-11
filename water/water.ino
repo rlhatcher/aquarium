82a966b6e44f5143238f86abf722b3e8ef51b994
Ronald Hatcher <ronaldhatcher@mac.com>
2023-06-11 12:11:19 +0100

    button layout

--- water/water.ino
+++ water/water.ino
@@ -1,6 +1,6 @@
+#include <Adafruit_FT6206.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
-#include <Adafruit_FT6206.h>
 
 #define TFT_CS 10
 #define TFT_DC 9
@@ -17,27 +17,25 @@ int buttonHeight = 60;
 int buttonWidth;
 int buttonTop;
 
-int relayPins[3] = {13, 12, 11}; // Inlet, Rinse, Pump
-int productFlowPin = 2; // Product flow sensor
-int wasteFlowPin = 3; // Waste flow sensor
+int relayPins[3] = {13, 12, 11};
+int productFlowPin = 2;
+int wasteFlowPin = 3;
 
-struct buttonCtrl
-{
+struct buttonCtrl {
   String label;
   int pin;
   boolean state;
 };
 
-struct sensorCtrl
-{
+struct sensorCtrl {
   String label;
   int pin;
-  volatile int * flowCounter;
+  volatile int* flowCounter;
 };
 
 buttonCtrl buttons[NUM_BUTTONS] = {
-  {"Inlet", 13, false},
-  {"Rinse", 12, true},
+  {"Feed", 13, false}, 
+  {"Purge", 12, true}, 
   {"Pump", 11, false}
 };
 
@@ -54,7 +52,7 @@ void productFlowInterrupt() {
 }
 
 void wasteFlowInterrupt() {
-  wasteFlowCounter++;
+  wasteFlowCounter++; 
 }
 
 void setup() {
@@ -70,29 +68,31 @@ void setup() {
 
   tft.begin();
 
-  // Rotate the screen 90 degrees 
+  // Rotate the screen 90 degrees
   tft.setRotation(3);
   tft.fillScreen(ILI9341_BLACK);
-  
+
   // Pass in 'sensitivity' coefficient
-  if (!cts.begin(40)) { 
+  if (!cts.begin(40)) {
     Serial.println("Unable to start touchscreen.");
-  } 
-  else { 
-    Serial.println("Touchscreen started."); 
+  } else {
+    Serial.println("Touchscreen started.");
   }
 
   buttonWidth = tft.width() / NUM_BUTTONS;
   buttonTop = tft.height() - buttonHeight;
-  tft.setTextSize(2);
+  tft.setTextSize(3);
+  // tft.setFont(&FreeMonoBoldOblique12pt7b);
 
   for (int i = 0; i < NUM_BUTTONS; i++) {
+    int padding = (buttons[i].label.length() == 4) ? 20 : 10;
     tft.drawRect(i * buttonWidth, buttonTop, buttonWidth, buttonHeight, ILI9341_WHITE);
     uint16_t color = buttons[i].state ? ILI9341_DARKGREEN : ILI9341_RED;
-    tft.fillRect(i * buttonWidth+1, buttonTop+1, buttonWidth-1, buttonHeight-1, color);
-    tft.setCursor(i * buttonWidth + 10, buttonTop + 10);
+    tft.fillRect(i * buttonWidth + 1, buttonTop + 1, buttonWidth - 1, buttonHeight - 1, color);
+    tft.setCursor(i * buttonWidth + padding, buttonTop + 20);
     tft.println(buttons[i].label);
   }
+  tft.setFont();
 }
 
 void loop() {
@@ -108,31 +108,35 @@ void loop() {
 
   // Display the new sensor readings
   tft.setCursor(0, 0);
-  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(3);
   for (int i = 0; i < NUM_SENSORS; i++) {
     tft.print(sensors[i].label);
-    tft.print(":");
-    tft.print(*sensors[i].flowCounter/5.88);
+    tft.print("  ");
+    tft.print(*sensors[i].flowCounter / 5.88);
     tft.println(" mL");
   }
 
   // Check for touch events and handle them
   if (cts.touched()) {
     TS_Point p = cts.getPoint();
-    Serial.print("X = "); Serial.print(p.x);
-    Serial.print("\tY = "); Serial.println(p.y);
+    Serial.print("X = ");
+    Serial.print(p.x);
+    Serial.print("\tY = ");
+    Serial.println(p.y);
 
     for (int i = 0; i < 3; i++) {
-      if (p.x >= i * buttonWidth && p.x < (i+1) * buttonWidth &&
+      if (p.x >= i * buttonWidth && p.x < (i + 1) * buttonWidth &&
           p.y >= tft.height() / 2 && p.y < tft.height() / 2 + buttonHeight) {
-        // The touch event was within the area of button i. Toggle the state of relay i.
+        // The touch event was within the area of button i. Toggle the state of
+        // relay i.
         bool newState = !digitalRead(relayPins[i]);
         digitalWrite(relayPins[i], newState);
 
         // Update the color of the button to indicate the new state
         uint16_t color = newState ? ILI9341_GREEN : ILI9341_RED;
-        tft.fillRect(i * buttonWidth, tft.height() / 2, buttonWidth, buttonHeight, color);
+        tft.fillRect(i * buttonWidth, tft.height() / 2, buttonWidth,
+                     buttonHeight, color);
       }
     }
   }
